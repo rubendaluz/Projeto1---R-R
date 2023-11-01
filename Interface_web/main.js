b22e5d404fe17dbb6fd32e2e905ed141984eeb9f
Ruben da Luz <rubendaluz684@gmail.com>
2023-11-01 16:59:24 +0000

    v1.4

--- Interface_web/main.js
+++ Interface_web/main.js
@@ -7,6 +7,11 @@ const li_accesses = document.querySelector("#li_accesses");
 const users_container = document.querySelector(".users_container");
 const rooms_container = document.querySelector(".rooms_container");
 const accesses_container = document.querySelector(".accesses_container");
+const open_new_user_form_button = document.querySelector("#create_user_button");
+const new_user_form = document.querySelector(".new_user_form");
+const open_new_room_form_button = document.querySelector("#create_room_button");
+const new_room_form = document.querySelector(".new_room_form");    
+
 
 li_users.addEventListener("click", (e) => {
     li_users.style.background = "grey"; 
@@ -15,6 +20,7 @@ li_users.addEventListener("click", (e) => {
     users_container.style.display = "block";
     accesses_container.style.display = "none";
     rooms_container.style.display = "none"
+    new_room_form.style.display = "none"
 });
 
 li_rooms.addEventListener("click", (e) => {
@@ -24,6 +30,7 @@ li_rooms.addEventListener("click", (e) => {
     users_container.style.display = "none"
     accesses_container.style.display = "none";
     rooms_container.style.display = "block"
+    new_user_form.style.display = "none"
 });
 
 li_accesses.addEventListener("click", (e) => {
@@ -35,10 +42,7 @@ li_accesses.addEventListener("click", (e) => {
     rooms_container.style.display = "none"
 });
 
-const open_new_user_form_button = document.querySelector("#create_user_button");
-const new_user_form = document.querySelector(".new_user_form");
-
-open_new_user_form_button.addEventListener("click", (e) => {
+open_new_user_form_button.addEventListener("click", () => {
     new_user_form.style.display = "block";
 });
 
@@ -49,39 +53,84 @@ new_user_form_close_button.addEventListener("click", (e) => {
 });
 
 let close_new_user_form = () => { 
-    document.getElementById('new_user_name').value = '';
+    document.getElementById('new_user_first_name').value = '';
+    document.getElementById('new_user_last_name').value = '';
     document.getElementById('new_user_email').value = '';
+    document.getElementById('new_user_phone_number').value = '';
     document.getElementById('new_user_permission_level').value = '';
     new_user_form.style.display = "none";
 }
 
-const add_new_user_button = document.querySelector("#add_new_user_button");
+    const add_new_user_button = document.querySelector("#add_new_user_button");
 
-add_new_user_button.addEventListener("click", (e) => {
-    const user_name = document.querySelector("#new_user_name").value;
-    const user_email = document.querySelector("#new_user_email").value;
-    const user_permission_level = document.querySelector("#new_user_permission_level").value;
-    if (!user_permission_level) {
-        console.log("Permissiao dever estar entre 0-5");
-    } else if (!user_name) {
-        console.log("Sem username");
-    } else if (!user_email) {
-        console.log("Sem email para o utilizador");
-    } else{
-        const user_password = generatePassword();// const user_encrypted_password = encryptPassword(user_password);
-        console.log(user_name, user_email, user_password);
-        // Adcicionar codigo que adiciona os dados a base de dados
-        //Adicionar users a tabela
-        add_user_to_table(user_name, user_email, user_permission_level);
-        close_new_user_form();
-        // Adicionar codigo que faz refresh a pagina para atualizar os novos dados introduzidos
-    }  
+    add_new_user_button.addEventListener("submit", (e) => {
+        const user_first_name = document.querySelector("#new_user_first_name").value;
+        const user_last_name = document.querySelector("#new_user_last_name").value;
+        const user_phone_number = document.querySelector("#new_user_phone_number").value;
+        const user_email = document.querySelector("#new_user_email").value;
+        const user_permission_level = document.querySelector("#new_user_permission_level").value;
+        if (!user_permission_level) {
+            console.log("Permissiao dever estar entre 0-5");
+        } else if (!user_first_name) {
+            console.log("Sem username");
+        } else if (!user_email) {
+            console.log("Sem email para o utilizador");
+        } else if (!user_last_name) {
+            console.log("Ultimo nome não introduzido")
+        } else if (!user_phone_number) {
+            console.log("Ultimo nome não introduzido")
+        } else{
+            // Adcicionar os dados a base de dados
+            // URL para a qual você deseja fazer a solicitação POST
+            const url = 'https://localhost/api/login';
 
+            // Dados que você deseja enviar no corpo da solicitação
+            const data = {
+                firstName: user_first_name,
+                lastName: user_last_name,
+                email: user_email,
+                phone: user_phone_number,
+                accessLevel: user_permission_level,
+            };
 
-    const open_new_room_form_button = document.querySelector("#create_room_button");
-    const new_room_form = document.querySelector(".new_room_form");
+            // console.log(data);
 
-    open_new_room_form_button.addEventListener("click", (e) => {
+            // // Configuração da solicitação
+            // const options = {
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json' // Tipo de conteúdo do corpo da solicitação
+            // },
+            //     body: JSON.stringify(data) // Converte os dados em JSON
+            // };
+
+            // // Faz a solicitação usando o fetch
+            // fetch(url, options)
+            //     .then(response => {
+            //         if (response.ok) {
+            //             return response.json(); // Se a resposta for bem-sucedida, analise a resposta JSON
+            //         } else {
+            //             throw new Error('Falha na solicitação POST');
+            //         }
+            // })
+            // .then(data => {
+            //     console.log(data); // Faça algo com os dados de resposta
+            // })
+            // .catch(error => {
+            //     console.error(error); // Trate erros de solicitação
+            // });
+
+            //Adicionar users a tabela
+            add_user_to_table(user_first_name,user_last_name,user_email,user_phone_number,user_permission_level);
+            close_new_user_form();
+            // Adicionar codigo que faz refresh a pagina para atualizar os novos dados introduzidos
+        }  
+    });
+    
+
+   open_new_room_form_button.addEventListener("click", (e) => {
+        console.log("aaaa");
+        e.preventDefault()
         new_room_form.style.display = "block";
     })
 
@@ -101,7 +150,7 @@ add_new_user_button.addEventListener("click", (e) => {
 
     add_new_room_button.addEventListener("click", (e) => {
         const room_name = document.querySelector("#new_room_name").value;
-        const room_security_level = document.querySelector("#new_room_email").value;
+        const room_security_level = document.querySelector("#new_room_security_level").value;
         
         if (!room_name) {
             console.log("Sem username");
@@ -109,14 +158,13 @@ add_new_user_button.addEventListener("click", (e) => {
             console.log("Sem email para o utilizador");
         } else{
             console.log(room_name,room_security_level)
-            // Adcicionar codigo que adiciona os dados a base de dados
+            add_room_to_table(room_name,room_security_level)
             //Adicionar users a tabela
             // add_room_to_table(room_name, room_security_level);
             close_new_room_form();
             // Adicionar codigo que faz refresh a pagina para atualizar os novos dados introduzidos
         }  
     });
-});
 
 })
 
@@ -124,20 +172,33 @@ let generatePassword = () => {
     return Math.random().toString(36).slice(-8);
 }
 
-
-function closeForm() {
-    // Close the form by hiding it
-    document.querySelector('.form-container').style.display = 'none';
-}
-
-let add_user_to_table = (username, useremail, permission_level) => {
+let add_user_to_table = (firstName,lastName,email,phone,accessLevel) => {
     const table_body = document.querySelector("#table_body");
+    const name = firstName + " " + lastName;
+
 
     table_body.innerHTML += `
         <tr>
-            <td>${username}</td>
-            <td>${useremail}</td>
-            <td>${permission_level}</td>
+            <td>${name}</td>
+            <td>${email}</td>
+            <td>${phone}</td>
+            <td>${accessLevel}</td>
+            <td class="action-icons">
+                <a href="#" title="Edit"><i class="fas fa-edit"></i></a>
+                <a href="#" title="Delete"><i class="fas fa-trash-alt"></i></a>
+            </td>
+        </tr>
+    `
+}
+
+let add_room_to_table = (roomname, roomseclevel) => {
+    const table_body = document.querySelector("#rooms_table_body");
+
+    table_body.innerHTML += `
+        <tr>
+            <td>2</td>
+            <td>${roomname}</td>
+            <td>${roomseclevel}</td>
             <td class="action-icons">
                 <a href="#" title="Edit"><i class="fas fa-edit"></i></a>
                 <a href="#" title="Delete"><i class="fas fa-trash-alt"></i></a>
