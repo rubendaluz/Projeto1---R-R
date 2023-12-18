2ca2967cdd7aba3c42e20de95b16b301a8410970
RomilsonMonteiro <132623804+RomilsonMonteiro@users.noreply.github.com>
2023-12-18 13:31:23 +0000

    edit and delete user

--- Servidor_Backend/Server/routes/accesses.routes.js
+++ Servidor_Backend/Server/routes/accesses.routes.js
@@ -11,19 +11,19 @@ const accessesRoutes = Router();
 accessesRoutes.post('/create', createAccess);
 
 // Rota para buscar acessos por usuário
-router.get('/:userId', getAccessesByUser);
+accessesRoutes.get('/:userId', getAccessesByUser);
 
 // Rota para buscar acessos por data
-router.get('/:date', getAccessesByDate);
+accessesRoutes.get('/:date', getAccessesByDate);
 
 // Rota para buscar acessos por hora
-router.get('/:hour', getAccessesByHour);
+accessesRoutes.get('/:hour', getAccessesByHour);
 
 // Rota para buscar acessos por área
-router.get('/:areaId', getAccessesByArea);
+accessesRoutes.get('/:areaId', getAccessesByArea);
 
 // Rota para buscar todos os acessos
-router.get('/', getAllAccesses);
+accessesRoutes.get('/', getAllAccesses);
 
 
 export { accessesRoutes };
